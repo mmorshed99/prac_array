d75e47550c5888390fa0c69638d081bf8708e815
M Manzur Morshed <mmorshed@asu.edu>
2018-03-19 22:07:53 -0400

    Update add one to number.py

--- add one to number.py
+++ add one to number.py
@@ -12,37 +12,62 @@
 #
 #as 123 + 1 = 124.
 
+#class Solution:
+#    # @param A : list of integers
+#    # @return a list of integers
+#    def plusOne(self, A):
+#       B = []
+#       C = []
+#       tag_0 = 0
+#       for i in range(0,len(A)):
+#           if A[i] == 0 and tag_0 ==0:
+#               if A[-1] == 0:
+#                   B.append(1)
+#               continue
+#           else:
+#               tag_0 = 1
+#           B.append(A[i])
+#           if i == len(A) - 1:
+#               B[-1] += 1
+#               if B[-1] == 10:
+#                   count = 2
+#                   B[-1] = 0
+#                   j = len(B) - 2
+#                   while (count < (len(B) + 1)):
+#                       count += 1
+#                       B[j] = B[j] + 1
+#                       if B[j] != 10:
+#                           break
+#                       
+#                       B[j] = 0
+#                       j = j -1
+#                   if B[0] == 10:
+#                       B[0] = 0
+#                   if B[0] == 0:
+#                       B.insert(0,1)
+#       return B               
 class Solution:
     # @param A : list of integers
     # @return a list of integers
     def plusOne(self, A):
-       B = []
-       C = []
-       tag_0 = 0
-       for i in range(0,len(A)):
-           if A[i] == 0 and tag_0 ==0:
-               if A[-1] == 0:
-                   B.append(1)
-               continue
-           else:
-               tag_0 = 1
-           B.append(A[i])
-           if i == len(A) - 1:
-               B[-1] += 1
-               if B[-1] == 10:
-                   count = 2
-                   B[-1] = 0
-                   j = len(B) - 2
-                   while (count < (len(B) + 1)):
-                       count += 1
-                       B[j] = B[j] + 1
-                       if B[j] != 10:
-                           break
-                       
-                       B[j] = 0
-                       j = j -1
-                   if B[0] == 10:
-                       B[0] = 0
-                   if B[0] == 0:
-                       B.insert(0,1)
-       return B               
+        carry = 1
+        B = []
+        C = []
+        for i in reversed(range(len(A))):
+            temp = int(A[i]) + carry
+            if temp > 9:
+                carry = 1
+                temp_lst = list(str(temp))
+                B.append(int(temp_lst[1]))
+            else:
+                carry = 0
+                B.append(temp)
+        if carry == 1:
+            B.append(1)
+        zero_remove = 0
+        for i in reversed(range(len(B))):
+            if B[i] != 0:
+                zero_remove = 1
+            if zero_remove:
+              C.append(B[i])
+        return C
